71034ef179bc490403f81cbf8f89616dae95965c
Gulka_D <d.gulka4@gmail.com>
2021-06-24 14:35:07 +0200

    add colors to the ouput

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const franc = require('franc');
 const langs = require("langs");
+const colors = require('colors');
 
 const arg = process.argv[2];
 
 
 if (franc(arg) === "und") {
-    console.log('Sentence is too short. Pls pass a longer sentence to detect the language.');
+    console.log('Sentence is too short. Pls pass a longer sentence to detect the language.'.red);
 }
 else {
-    console.log(langs.where('3', franc(arg)).name);
+    console.log(`Our best guess is: ${langs.where('3', franc(arg)).name.green}`);
 };
