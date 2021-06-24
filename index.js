7f1d1d148fe42beace4711afbd46d5f4eddb5117
Gulka_D <d.gulka4@gmail.com>
2021-06-24 14:23:52 +0200

    Final effect, Add langs package.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const franc = require('franc');
+const langs = require("langs");
 
 const arg = process.argv[2];
 
 
 if (franc(arg) === "und") {
     console.log('Sentence is too short. Pls pass a longer sentence to detect the language.');
-} 
+}
 else {
-    console.log(franc(arg));
+    console.log(langs.where('3', franc(arg)).name);
 };
